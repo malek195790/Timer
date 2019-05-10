ee0428784c3917071b30d000f69795b53132759f
malek195790 <50481184+malek195790@users.noreply.github.com>
2019-05-10 09:40:03 +0100

    static timer

---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Timer = () => 
+    <div className="Time-style">
+        <h1>00:30:00</h1>
+        <p>
+            <span>Hour</span>
+            <span>Minute</span>
+            <span>Second</span>
+        </p>
+    </div>
+
+export default Timer;
